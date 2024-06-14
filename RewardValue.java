c6595b16bd228a43214eea5e3806276696fb8478
VithujaThangarajah <vithuja.thangarajah@city.ac.uk>
2024-06-14 17:47:35 +0100

    Task 1: RewardValue

---
+++ RewardValue.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.util.*;
+
+public class RewardValue {
+    private final double cashValue;
+    private static final double MILES_TO_CASH_RATE = 0.0035;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_RATE;
+    }
+
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
+}
